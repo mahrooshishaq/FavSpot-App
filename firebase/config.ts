d790afbbf39723c71d7e1450e40cc9f9bdd80759
mahrooshishaq <mahrooshishaq@gmail.com>
2025-07-03 14:03:44 +0500

    Initial setup complete, firebase testing done

---
+++ firebase/config.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOlKAg395Dg9MGZYCQH_2k8VHpZhbTi2o",
+  authDomain: "favspotsapp.firebaseapp.com",
+  projectId: "favspotsapp",
+  storageBucket: "favspotsapp.firebasestorage.app",
+  messagingSenderId: "111140303619",
+  appId: "1:111140303619:web:a29723fde12c5558a86f36",
+  measurementId: "G-PVMSYHXJJB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
